38413662aba45c61538ef6a00f0eee573f55ef14
Dave Hurst <dhurst79@gmail.com>
2014-09-18 22:49:03 -0700

    fix Reset button (now starts at 1 throw)

--- server.R
+++ server.R
@@ -6,15 +6,17 @@ throw.history <- numeric()
 set.seed <- 1999
 last.reset <- 0
 n.throws <- 0
+hide.quick.start <- 0
+show.explanation <- 0
 
 shinyServer(
      function(input, output) {
           output$image <- renderPlot({
                if( input$resetButton > last.reset)  {
                     isolate({ 
-                         throw.history<<-throw.history[1]
+                         throw.history<<-numeric()
                          last.reset <<- input$resetButton
-                         n.throws <<- -1
+                         n.throws <<- 0
                     })
                }
                input$goButton
@@ -63,6 +65,39 @@ shinyServer(
                     lines(c(pi, pi), c(0, hist.max),col="black",lty=2)
                     text(pi, hist.max*.8, expression(pi), adj=c(-1,0))
                } ) #isolate
-          }, width=600, height=800)  #renderPlot
+          }, width=400, height=800)  #renderPlot
+          
+          output$quick.start <- renderText({
+               hide <- input$hideQuick %% 2  #hide if the number of show/hide clicks is odd
+               
+               quick.text <- '<h4>Quick Start</h4>
+                <li>Leave the default values for now (100 grain/1 throw)</li>
+                <li>Hit the "Throw" button a few times</li>
+                <li>Notice the histogram forming, and the estimated value of pi (red line) converging to the actual (black line)
+                <li>Use the slider to increase the number of grains per throw</li>
+                <li>Use the number of throws to speed up the growth of the histogram</li>
+               '
+               ifelse(hide, "", quick.text)
+          })
+          #if( input$hideQuick ) { output$quick.start <- ""}
+
+          output$explain <- renderText({ 
+                show <- input$hideExplain %% 2  #show if the number of show/hide clicks is odd
+               explain.text <- "
+               <div>The motivation for this gadget comes from 
+               <a href='http://en.wikipedia.org/wiki/Monte_Carlo_method'>Wikipedia - Monte Carlo Method</a>
+               (see Introduction)</div>
+               <br><b>Try this warm up question to get you going:</b>
+               <br>Q: What if you were asked to find the value of pi using just a can of beans, a string and a piece of college ruled notepaper?
+               <br>A: Easy.  You wrap the string around the can and measure it to get the circumference (using the lines on the ruled paper), measure the diameter of can, and divide the circumfance by the diameter to get pi.
+               <p>Q: Now what if you had to find pi using only some grains of quinoa?
+               <br>A: You construct a diagram of a square with a circle circumscribed inside (like what's on the right hand side).
+               Now throw some quinoa grains randomly and count the ones that land in the circle and divide it by the total thrown (assuming they all landed somewhere in the square) to get the ratio.
+               The probability that a grain lands inside the circle is the area of the circle divided by the area of the square.
+               Substituting the ratio above for the probabilty and rearranging a bit gives us an estimate for pi (see the formula to the right).
+               Unfortunately, its not a rapidly converging estimate and you need to count a lot of quinoa -- or you can just run this handy app.
+               </div>"
+               ifelse(show, explain.text, " ")
+           })
      }
 )
